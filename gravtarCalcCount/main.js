b012042033a36f716dc4630604137fc0c92722c2
Son Truong <sont85@gmail.com>
2015-06-24 00:12:35 -0700

    gravtar calculator count homework completed

---
+++ gravtarCalcCount/main.js
@@ -0,0 +1,65 @@
+var fs = require("fs"),
+    http = require("http"),
+    request = require('request'),
+    url = require('url'),
+    exec = require("child_process").exec,
+    md5 = require("MD5");
+
+var math = {
+  "+": function(a,b) {
+    return a+b;
+  },
+    "-": function(a,b) {
+    return a-b;
+  },
+    "*": function(a,b) {
+    return a*b;
+  },
+    "/": function(a,b) {
+    return a/b;
+  }
+}
+http.createServer(responseHandler).listen(8888);
+
+function responseHandler(req, res) {
+  if (req.url.match("fav")) {
+    res.end("");
+    return;
+  }
+  if (req.url === "/") {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', 'utf8', function (err,data) {
+      res.end(data);
+    });
+  } else if (req.url.substr(0,13) === "/gravatarUrl/"){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    console.log(req.url.substr(13));
+    console.log(md5(req.url.substr(13)))
+    res.end("Gravatar URL: http://www.gravatar.com/avatar/"+md5(req.url.substr(13)))
+  } else if (req.url.substr(0,6) === "/Calc/") {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    req.url = req.url.substr(6)
+    var arithmeticExpression = req.url 
+    var firstNumber = parseInt(req.url.match(/^\d+/))
+    var secondNumber = parseInt(req.url.match(/\d+$/))
+    var answer = math[req.url.replace(/\d+/g, "")](firstNumber,secondNumber).toString()
+    res.end(arithmeticExpression +"= "+ answer)
+  } else if (req.url.substr(0,8) === "/Counts/") {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    req.url = decodeURI(req.url)
+    req.url = req.url.substr(8);
+    console.log(req.url);
+    var letterCount = req.url.match(/[A-z]/g).length;
+    var spaceCount = req.url.match(/\s/g).length;
+    var wordCount = req.url.match(/\w+/g).length
+    var count = {"letterCount": letterCount, "spaceCount": spaceCount, "wordCount": wordCount}
+    res.end(JSON.stringify(count))
+  } else {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    request('http://points.agilelabs.com'+req.url+'.json', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.end(body);
+      }
+    });
+  }
+}
