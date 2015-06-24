4b1b7bce5393610555bb6dd3f93ba3fbafc7c507
Son Truong <sont85@gmail.com>
2015-06-24 01:25:59 -0700

    added case insensitive

--- gravtarCalcCount/main.js
+++ gravtarCalcCount/main.js
@@ -20,21 +20,21 @@ var math = {
   }
 }
 http.createServer(responseHandler).listen(8888);
-
 function responseHandler(req, res) {
   if (req.url.match("fav")) {
     res.end("");
     return;
   }
+  req.url = req.url.toLowerCase()
   if (req.url === "/") {
     res.writeHead(200, {"Content-Type": "text/html"});
     fs.readFile('index.html', 'utf8', function (err,data) {
       res.end(data);
     });
-  } else if (req.url.substr(0,13) === "/gravatarUrl/"){
+  } else if (req.url.substr(0,13) === "/gravatarurl/"){
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Gravatar URL: http://www.gravatar.com/avatar/"+md5(req.url.substr(13)))
-  } else if (req.url.substr(0,6) === "/Calc/") {
+  } else if (req.url.substr(0,6) === "/calc/") {
     res.writeHead(200, {"Content-Type": "text/plain"});
     req.url = req.url.substr(6);
     var arithmeticExpression = req.url ;
@@ -42,7 +42,7 @@ function responseHandler(req, res) {
     var secondNumber = parseInt(req.url.match(/\d+$/));
     var answer = math[req.url.replace(/\d+/g, "")](firstNumber,secondNumber).toString()
     res.end(arithmeticExpression +"= "+ answer);
-  } else if (req.url.substr(0,8) === "/Counts/") {
+  } else if (req.url.substr(0,8) === "/counts/") {
     res.writeHead(200, {"Content-Type": "text/plain"});
     req.url = decodeURI(req.url);
     req.url = req.url.substr(8);
